763f5c9984e99ab1c0c03c2ff3a64f0b9253eddd
hiroyukisugiura-invitation <from.invitation@gmail.com>
2025-07-24 16:19:06 +0900

    Update script.js

--- public/work/illustration/script.js
+++ public/work/illustration/script.js
@@ -1,11 +1,15 @@
 function changeMannequin(type) {
   const mannequin = document.getElementById("mainMannequin");
   const basePath = "../../../mannequin/";
+
   if (type === "woman") {
     mannequin.src = basePath + "mannequin_woman.png";
+    mannequin.style.height = "580px";
   } else if (type === "man") {
     mannequin.src = basePath + "mannequin_man.png";
+    mannequin.style.height = "630px"; // 約109%
   } else if (type === "kids") {
     mannequin.src = basePath + "mannequin_kids.png";
+    mannequin.style.height = "360px"; // 約62.5%
   }
 }
