ed4f57936029bd10d92f9608627b95d687c762f8
hiroyukisugiura-invitation <from.invitation@gmail.com>
2025-07-24 22:05:00 +0900

    Update script.js

--- public/work/illustration/script.js
+++ public/work/illustration/script.js
@@ -1,1 +1,19 @@
 
+// マネキン切替処理
+const mannequin = document.getElementById("mannequin");
+
+const womanBtn = document.getElementById("womanBtn");
+const manBtn = document.getElementById("manBtn");
+const kidsBtn = document.getElementById("kidsBtn");
+
+womanBtn.addEventListener("click", () => {
+  mannequin.src = "../../mannequin/mannequin_woman.png";
+});
+
+manBtn.addEventListener("click", () => {
+  mannequin.src = "../../mannequin/mannequin_man.png";
+});
+
+kidsBtn.addEventListener("click", () => {
+  mannequin.src = "../../mannequin/mannequin_kids.png";
+});
