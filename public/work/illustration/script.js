ec02187613bd6771ac3ef874314bd23bbb157570
hiroyukisugiura-invitation <from.invitation@gmail.com>
2025-07-24 11:41:07 +0900

    Update script.js

--- public/work/illustration/script.js
+++ public/work/illustration/script.js
@@ -1,1 +1,12 @@
+function changeMannequin(type) {
+  const mannequin = document.getElementById("mainMannequin");
+  const basePath = "../../../mannequin/";
+  if (type === "woman") {
+    mannequin.src = basePath + "mannequin_woman.png";
+  } else if (type === "man") {
+    mannequin.src = basePath + "mannequin_man.png";
+  } else if (type === "kids") {
+    mannequin.src = basePath + "mannequin_kids.png";
+  }
+}
 
